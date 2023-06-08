490a7aef444685d11ddc87844c440e9458a37b92
Tom Fontaine <tom.fnt9@gmail.com>
2023-06-08 12:17:27 +0200

    Liaison Front Back

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+const datalist = document.querySelector("#datalist");
+const question = document.querySelector(".messquestion");
+const answer = document.querySelector(".messreponse");
+const inputdialog = document.querySelector("#sugestion");
+
+
+fetch("https://gregbot-back.onrender.com/api/v1/dialogs")
+    .then(response=>response.json())
+    .then(data=>{
+        data.message.forEach(dialog => {
+            datalist.innerHTML += `<option value="${dialog.question}">`
+        })
+
+        inputdialog.addEventListener("change", e=>{
+            question.innerHTML=document.querySelector(".form-question").value
+            data.message.forEach(dialog =>{
+                if(e.target.value === dialog.question){
+                    console.log(dialog.answer)
+                    answer.innerHTML = dialog.answer
+                }
+            })
+        })
+    })
+    .catch(error=>console.log(error))
