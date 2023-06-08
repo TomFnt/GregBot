83c637059a318f726201206bed9163b1fc2c2813
Tom Fontaine <tom.fnt9@gmail.com>
2023-06-08 12:50:06 +0200

    fix chatbox hmtl & app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,18 @@
 const datalist = document.querySelector("#datalist");
 const question = document.querySelector(".messquestion");
 const answer = document.querySelector(".messreponse");
-const inputdialog = document.querySelector(".question");
-
+const inputdialog = document.querySelector("#question");
 
 fetch("https://gregbot-back.onrender.com/api/v1/dialogs")
     .then(response=>response.json())
-    .then(data=>{
-        data.message.forEach(dialog => {
+    .then(({data})=>{
+        data.forEach(dialog => {
             datalist.innerHTML += `<option value="${dialog.question}">`
         })
 
         inputdialog.addEventListener("change", e=>{
-            question.innerHTML=document.querySelector(".form").value
-            data.message.forEach(dialog =>{
+            question.innerHTML=document.querySelector(".question").value
+            data.forEach(dialog =>{
                 if(e.target.value === dialog.question){
                     console.log(dialog.answer)
                     answer.innerHTML = dialog.answer
